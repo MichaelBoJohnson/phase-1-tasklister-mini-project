2efe0f1cfb7c0e294bca5f2d0442620eb96b1858
Michael Johnson <michaelbojohnson@gmail.com>
2022-03-19 15:17:48 -0500

    added ability to add things to task lister lite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // deliverbles:
+  //1.) user should be able to type a task into input field
+  //2.) user should be able to click some form of submit button
+  //3.) user should be able to see the task string that they provided appear in the DOM after the submit button has been activated
+    document.querySelector('form').addEventListener('submit', (e) => {
+      e.preventDefault();
+      handleTodo(e.target.new_task_description.value);
+    });
+
+      function handleTodo(todo){
+        let p = document.createElement('p')
+        p.textContent = todo;
+        document.querySelector('#list').appendChild(p)
+        console.log(p)
+      }
+
 });
